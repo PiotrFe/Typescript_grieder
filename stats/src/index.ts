3c8598d81e52f0c48b9f925b7b6c0e743eb48af5
Piotr Ferensztajn <Piotr_Ferensztajn@MCKINSEY.COM>
2020-12-17 19:01:17 +0100

    lesson

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,18 @@
 import { MatchReader } from "./MatchReader";
-import {CsvFileReader} from "./CsvFileReader";
-import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 
 matchReader.load();
 
-let manUnitedWins = 0;
+const summary = new Summary(
+    new WinsAnalysis("Man United"), 
+    new HtmlReport()
+)
 
-
-
-console.log(manUnitedWins);
+summary.buildAndPrintReport(matchReader.matches);
