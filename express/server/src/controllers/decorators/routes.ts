955e3df7caae0a3b95c880984bae68fcd81ec1d2
Piotr Ferensztajn <Piotr_Ferensztajn@MCKINSEY.COM>
2021-01-03 23:47:45 +0100

    Lesson

---
+++ express/server/src/controllers/decorators/routes.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", "get", target, key);
+    };
+  }
+}
+
+export const get = routeBinder("get");
+export const post = routeBinder("post");
+export const put = routeBinder("put");
+export const del = routeBinder("delete");
+
